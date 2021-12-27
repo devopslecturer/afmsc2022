fa955c065f068af81dce230ce7546d5146ecf6c2
Aravind Rajesh Kanna <L00163466@student.lyit.ie>
2021-12-27 03:22:58 +0000

    Flask MVC setup

---
+++ controllers/UserController.py
@@ -0,0 +1,27 @@
+import sys
+from flask import render_template, redirect, url_for, request, abort
+
+from models.User import User
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+def index():
+    return render_template('users/index.html')
+
+
+def create():
+    pass
+
+
+def show(user_id):
+    pass
+
+
+def update(user_id):
+    pass
+
+
+def destroy(user_id):
+    pass
