ac60082c5b08ac7c469ae8bc4c55f21ae607f1e9
Bubashankushan <l00163287@student.lyit.ie>
2022-02-08 14:54:58 +0000

    DB Models & files updated to support tables creation.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
 
-from models.User import db
-from models.Signup import signup
+from models.DBModels import db
 from routes.user_bp import user_bp
 from routes.signup_bp import signup_bp
 from routes.login_bp import *
@@ -11,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 db.init_app(app)
-migrate = Migrate(app, db, signup)
+migrate = Migrate(app, db)
 
 app.register_blueprint(user_bp, url_prefix='/users')
 app.register_blueprint(signup_bp, url_prefix='/signup')
