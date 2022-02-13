3636fc069caf6a77e99a1af6978516f2eb938e54
Dalimol Abraham <L00169942@student.lyit.ie>
2022-02-13 20:24:48 +0000

    reset password

--- controllers/reset_password_controller.py
+++ controllers/reset_password_controller.py
@@ -21,6 +21,16 @@ def index():
     return render_template('reset_password/reset_password.html')
 
 
-def login():
-    pass
+def reset():
+    if request.method == 'POST':
+        if not request.form['email']:
+            flash('Please enter all the fields', 'error')
+        else:
+            if request.form['email'] == request.form['email']:
+                user_email = request.form['email'];
+                rest_pswd = db.relationship('userCred',email=user_email, lazy = True)
 
+                flash('Record was successfully added')
+            else:
+                flash('Passwords does not match', 'error')
+    return render_template('signup/index.html')
